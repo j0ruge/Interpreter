3a3f8a911bf76e9f0cea9047406f5eab1d6f88e6
Brenno Sciammarella <brenno.s97@gmail.com>
2022-05-01 15:19:14 -0300

    Organizacao

--- Program.cs
+++ Program.cs
@@ -4,116 +4,27 @@ namespace Interpreter
 {
     class Program
     {
-        static int menu_op = 0;             //valor para seleção do menu
-        static int code_op = 0;             //valor para seleção da operação
-        static int registrador_a = 0;       //registrador A
-        static int registrador_b = 0;       //registrador B
-        static char reg_op = 'A';           //usado para recursividade das seleções de registrador
+        static int menu_op = 0;                 //valor para seleção do menu
+        static int code_op = 000;                 //valor para seleção da operação
+        static short registrador_a = 0;         //registrador A
+        static short registrador_b = 0;         //registrador B
+        static char reg_op = 'A';               //usado para recursividade das seleções de registrador
 
-        static int program_counter; // contador de programa contém endereço da próxima instruction
-        static int accumulator; // o acumulador, um registrador para efetuar aritmética
-        static int instruction; // um registrador para conter a instrução corrente
-        static int instr_type; // o tipo da instrução (opcode)
-        static int data_loc; // o endereço dos dados, ou –1 se nenhum
-        static int data; // mantém o operando corrente
-        static bool run_bit = false; // um bit que pode ser desligado para parar a máquina
-        const int CLR = 1000;// <-- seta o valor no accumulator para 0
-        const int ADDI = 1005;// <-- adiciona o valor x no accumulator
-        const int ADDM = 1010;// <-- adiciona o valor da memória y no accumulator
-        const int HALT = 1100;// <-- instrução que desliga o processador
+        static bool run_bit = true;            // um bit que pode ser desligado para parar a máquina
 
-        public static void interpret(int[] memory, int starting_address)
-        {
-            /**
-             * Esse procedimento interpreta programas para uma máquina simples com instruções que têm
-             * um operando na memória. A máquina tem um registrador accumulator, usado para
-             * aritmética. A instrução ADD soma um inteiro na memória do accumulator, por exemplo.
-             * O interpretador continua funcionando até o bit de funcionamento ser desligado pela instrução HALT.
-             * O estado de um processo que roda nessa máquina consiste em memória, o
-             * contador de programa, bit de funcionamento e accumulator. Os parâmetros de entrada consistem 
-             * na imagem da memória e no endereço inicial.
-             */
-
-            program_counter = starting_address;
-            run_bit = true;
-            while (run_bit)
-            {
-                instruction = memory[program_counter]; // busca a próxima instrução e armazena em instruction
-                program_counter = program_counter + 1; // incrementa contador de programa
-                instr_type = get_instr_type(instruction); // determina tipo da instrução
-                data_loc = find_data(instruction, instr_type, memory); // localiza dados (–1 se nenhum)
-                if (data_loc >= 0) // se data_loc é –1, não há nenhum operando
-                { data = memory[data_loc]; } // busca os dados
-                execute(instr_type, data); // executa instrução
-            }
-        }
-
-
-        private static int get_instr_type(int opcode)
-        {
-            return opcode;
-        }
-        private static int find_data(int opcode, int type, int[] memory)
-        {
-            if (opcode == ADDI)
-            {
-                return program_counter;
-            }
-            if (opcode == ADDM)
-            {
-                return memory[program_counter];
-            }
-            else
-                return -1;
-        }
-        private static void execute(int instr_type, int data)
-        {
-            if (instr_type == CLR)
-            {
-                accumulator = 0;
-                Console.WriteLine(accumulator);
-            }
-            if (instr_type == ADDI)
-            {
-                accumulator = accumulator + data;
-                Console.WriteLine(accumulator);
-            }
-            if (instr_type == ADDM)
-            {
-                accumulator = accumulator + data;
-                Console.WriteLine(accumulator);
-            }
-            if (instr_type == HALT)
-            {
-                run_bit = false;
-            }
-
-        }
+        const int CLR = 90;// <-- seta o valor no accumulator para 0
+        const int ADDI = 95;// <-- adiciona o valor x no accumulator
+        const int ADDM = 93;// <-- adiciona o valor da memória y no accumulator
+        const int HALT = 100;// <-- instrução que desliga o processador
 
         static void Main(string[] args)
         {
-			//int[] m2 = { 2, -5, 15, CLR, // o "programa" inicia aqui
-			//	ADDI, 12, ADDI, 7, ADDM, 0, ADDM, 1, CLR, HALT };
-			//Console.WriteLine("Imagem de memória 1: ");
-			//interpret(m2, 3);// start at CLR
-
-			//int[] m3 = { 1, 3, 5, CLR, // o "programa" inicia aqui
-			//	ADDI, 7, ADDM, 2, CLR, ADDM, 0, ADDM, 1, CLR, HALT };
-			//Console.WriteLine("Imagem de memória 2: ");
-			//interpret(m3, 3); // start at CLR
-
-			//int[] m4 = { 13, -5, 7, 8, CLR,//o "programa" inicia aqui
-			//	         ADDM, 1, 3, ADDM, 1, CLR, HALT
-   //         };
-			//Console.WriteLine("Imagem de memória 3: ");
-			//interpret(m4, 4);
-
             Menu();
         }
 
         static void Menu()
         {
-            while (menu_op != 8)
+            while (run_bit)
             {
                 Console.Clear();
                 Console.WriteLine("");
@@ -164,13 +75,19 @@ namespace Interpreter
                         Console.WriteLine("Ler registrador de flags");
                         break;
                     case 6:
-                        Operacao();
+                        SelecionarOperacao();
                         break;
                     case 7:
                         Console.Clear();
                         Console.WriteLine("Executar ULA");
+                        Execute();
                         Console.WriteLine("Aperte qualquer tecla para continuar");
                         Console.ReadLine();
+                        break;
+                    case 8:
+                        Console.WriteLine("");
+                        Console.WriteLine("HALT");
+                        run_bit = false;
                         break;
                 }
             }
@@ -180,7 +97,7 @@ namespace Interpreter
         {
             if(reg == 'A')
             {
-                Console.WriteLine("Definir registrador A");
+                Console.WriteLine("Definir registrador A => -32768 até 32767");
 
                 var retorno = Console.ReadLine();
                 if (retorno.Length == 0)
@@ -191,13 +108,12 @@ namespace Interpreter
                 }
                 else
                 {
-                    registrador_a = int.Parse(retorno);
-                }
-                    
+                    registrador_a = short.Parse(retorno);
+                }   
             }
             else
             {
-                Console.WriteLine("Definir registrador B");
+                Console.WriteLine("Definir registrador B => -32768 até 32767");
 
                 var retorno = Console.ReadLine();
                 if (retorno.Length == 0)
@@ -208,7 +124,7 @@ namespace Interpreter
                 }
                 else
                 {
-                    registrador_b = int.Parse(retorno);
+                    registrador_b = short.Parse(retorno);
                 }
             }
         }
@@ -235,7 +151,7 @@ namespace Interpreter
             }
         }
 
-        static void Operacao()
+        static void SelecionarOperacao()
         {
             Console.Clear();
             Console.WriteLine("");
@@ -255,9 +171,34 @@ namespace Interpreter
             var retorno = Console.ReadLine();
 
             if (retorno.Length == 0 || int.Parse(retorno) < 1 || int.Parse(retorno) > 8)
-                Operacao();
+                SelecionarOperacao();
             else
                 code_op = int.Parse(retorno);
+        }
+
+        private static void Execute()
+        {
+            
+            //if (code_op == 1)
+            //{
+            //    accumulator = 0;
+            //    Console.WriteLine(accumulator);
+            //}
+            //if (instr_type == ADDI)
+            //{
+            //    accumulator = accumulator + data;
+            //    Console.WriteLine(accumulator);
+            //}
+            //if (instr_type == ADDM)
+            //{
+            //    accumulator = accumulator + data;
+            //    Console.WriteLine(accumulator);
+            //}
+            //if (instr_type == HALT)
+            //{
+            //    run_bit = false;
+            //}
+
         }
     }
 }
