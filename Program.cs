63ca9e04c7c8f5b87a43725cba478346096b6890
j0ruge <46424602+j0ruge@users.noreply.github.com>
2022-06-19 16:54:48 -0300

    Adição do método de clear

--- Program.cs
+++ Program.cs
@@ -166,6 +166,11 @@ namespace Interpreter
                     IncrementA();
                     programCounter++;
                     break;
+                case CLEAR:
+                    Console.WriteLine("CLEAR");
+                    programCounter++;
+                    Clear();
+                    break;
                 case HALT:
                     Console.WriteLine("HALT");
                     Halt();
@@ -360,6 +365,23 @@ namespace Interpreter
         {
             run_bit = false;
         }
+
+        static void Clear()
+        {
+            ReturnOriginalState();
+        }
+
+        static void ReturnOriginalState()
+        {
+            registrador_A = "0000";
+            registrador_B = "0000";
+            instructorRegister = "0000";
+            flag_transbordo = false;
+            flag_negativo = false;
+            flag_zero = false;
+        }
+
+      
 
         //Utilizado para pegar a célula de memória desejada
         static string GetMemoryCell(string[] memory, int rowNumber)
